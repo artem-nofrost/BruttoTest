d7d9404e94412ea06c4fb35a528125993e7cb3bc
unknown <artem.peropov@mail.ru>
2022-07-29 18:35:26 +0700

    reg 0.96

--- src/server/repMax.js
+++ src/server/repMax.js
@@ -1,9 +1,11 @@
-export const repMax = {
-    id: 1,
-    name: 'Взятие в стойку',
-    units: 'кг',
-    value: 'racking',
-    max: 100,
-    min: 10,
-    defaultValue: 30,
-};
+export const repMax = [
+    {
+        id: 1,
+        name: 'Взятие в стойку',
+        units: 'кг',
+        value: 'racking',
+        max: 100,
+        min: 10,
+        defaultValue: 30,
+    },
+];
